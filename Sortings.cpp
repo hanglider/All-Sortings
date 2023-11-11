3f667d5ed331399ce22342e2b2fe8f84292e89df
Ivan Trofimov <trovimovbro@gmail.com>
2023-11-11 15:34:31 +0300

    upd 2

--- Sortings.cpp
+++ Sortings.cpp
@@ -6,10 +6,17 @@ using namespace std;
 
 template <typename T>
 void QuickSort(vector<T> &a);
-template <typename T>
+template <typename T>                   //have problems with cmp
 void QuickSort(vector<T> &a, T l, T r);
 template <typename T>
 void QuickSort(vector<T> &a, T l, T r, function<bool(T, T)> &cmp);
+
+template <typename T>
+void MergeSort(vector<T> &a);
+template <typename T>
+void MergeSort(vector<T> &a, T l, T r);
+template <typename T>
+void MergeSort(vector<T> &a, T l, T r, function<bool(T, T)> &cmp);
 
 template <typename T>
 void InsertionSort(vector<T>& a);
@@ -33,20 +40,23 @@ template <typename T>
 void RadixSort(vector<T>& arr, T l, T r, function<bool(T, T)> &cmp);*/
 
 template<typename T>
-void countSort(vector<T>& arr, T exp, T l, T r);
+void countSort(vector<T>& arr, T exp, T l, T r);  //for radix
 template <typename T>
-T G(vector<T>& a, T l, T r); //optimization for pick el in radix
+T G(vector<T>& a, T l, T r);                      //optimization for pick el in Quick
 template <typename T>
 auto F(vector<T>& a, T l, T r, function<bool(T, T)> &cmp); //sort for Quicksort
 
-
 template <typename T>
 static function<bool(T, T)> cmp = [](T a, T b) {
-    return a >= b;
+    return a < b;
 };
 
 template <typename T>
 T Binpow (T a, T n) {int r = 1;while (n) {if (n & 1) r *= a; a *= a; n >>= 1;} return r;}
+
+template<typename T>
+T findMax(vector<T>& arr, T l, T r) {T max = arr[l];
+    for (auto i = l + 1; i < r; i++) {if (arr[i] > max) max = arr[i];} return max;}
 
 /*
 
@@ -105,37 +115,13 @@ void QuickSort(vector<T> &a) {
 */
 
 template <typename T>
-void InsertionSort(vector<T>& a, T l, T r, function<bool(T, T)> &cmp){
-    for (auto i = l; i <= r; i++){
-        auto j = i - 1, key = a[i];
-        while (cmp(a[j], key) && j >= 0){//a[j] > key && j >= 0){
-            a[j + 1] = a[j];
-            j--;
-        }
-        a[j + 1] = key;
-    }
-}
-
-template <typename T>
-void InsertionSort(vector<T>& a, T l, T r){
-    return InsertionSort(a, l, r, cmp<typeof(a[0])>);
-}
-
-template <typename T>
-void InsertionSort(vector<T>& a){
-    return InsertionSort(a, (T)0, (T)a.size(), cmp<typeof(a[0])>);
-}
-
-
-
-template <typename T>
-void merge(vector<T>& a, T l, T mid, T r){
+void merge(vector<T>& a, T l, T mid, T r, function<bool(T, T)> &cmp){
     auto n1 = mid - l + 1;
     auto n2 = r - mid;
 
     auto i = l, j = mid + 1;
     while (i <= mid && j <= r) {
-        if (a[i] > a[j]) {
+        if (cmp(a[i], a[j])){//(a[i] > a[j]) {
             j++;
         } else {
             i++;
@@ -148,7 +134,7 @@ void merge(vector<T>& a, T l, T mid, T r){
     i = 0; j = 0;
     auto k = l;
     while (i < n1 && j < n2){
-        if (L[i] <= R[j]){
+        if (!cmp(L[i], R[j])){//(L[i] <= R[j]){
             a[k] = L[i];
             i++;
         } else {
@@ -170,39 +156,90 @@ void merge(vector<T>& a, T l, T mid, T r){
 }
 
 template <typename T>
-void MergeSort(vector<T>& a, T l, T r){
+void MergeSort(vector<T>& a, T l, T r, function<bool(T, T)> &cmp){
     if (l < r){
         auto mid = l + (r - l) / 2;
-        MergeSort(a, l, mid);
-        MergeSort(a, mid + 1, r);
-        merge(a, l, mid, r);
+        MergeSort(a, l, mid, ::cmp<typeof(a[0])>);
+        MergeSort(a, mid + 1, r, ::cmp<typeof(a[0])>);
+        merge(a, l, mid, r, ::cmp<typeof(a[0])>);
+    }
+}
+
+template <typename T>
+void MergeSort(vector<T>& a, T l, T r){
+    return MergeSort(a, l, r, cmp<typeof(a[0])>);
+}
+
+template <typename T>
+void MergeSort(vector<T>& a){
+    return MergeSort(a, (T)0, (T)a.size(), cmp<typeof(a[0])>);
+}
+
+/*
+
+    ///////////////////////////////////////
+
+*/
+
+template <typename T>
+void InsertionSort(vector<T>& a, T l, T r, function<bool(T, T)> &cmp){
+    for (auto i = l; i <= r; i++){
+        auto j = i - 1, key = a[i];
+        while (cmp(a[j], key) && j >= 0){//a[j] > key && j >= 0){
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = key;
+    }
+}
+
+template <typename T>
+void InsertionSort(vector<T>& a, T l, T r){
+    return InsertionSort(a, l, r, cmp<typeof(a[0])>);
+}
+
+template <typename T>
+void InsertionSort(vector<T>& a){
+    return InsertionSort(a, (T)0, (T)a.size(), cmp<typeof(a[0])>);
+}
+
+/*
+
+    ///////////////////////////////////////
+
+*/
+
+template <typename T>
+void MergeInsertionSort(vector<T>& a, T m, T l, T r, function<bool(T, T)> &cmp){
+    if (l < r){
+        auto mid = l + (r - l) / 2;
+        if (r- l <= m) {
+            InsertionSort(a, l, r, ::cmp<typeof(a[0])>);
+            return ;
+        }
+        MergeSort(a, l, mid, ::cmp<typeof(a[0])>);
+        MergeSort(a, mid + 1, r, ::cmp<typeof(a[0])>);
+        merge(a, l, mid, r, ::cmp<typeof(a[0])>);
     }
 }
 
 template <typename T>
 void MergeInsertionSort(vector<T>& a, T m, T l, T r){
-    if (l < r){
-        auto mid = l + (r - l) / 2;
-        if (r- l <= m) {
-            InsertionSort(a, l, r);
-            return ;
-        }
-        MergeSort(a, l, mid);
-        MergeSort(a, mid + 1, r);
-        merge(a, l, mid, r);
-    }
+    return MergeSort(a, l, r, cmp<typeof(a[0])>);
 }
 
-template<typename T>
-T findMax(vector<T>& arr, T l, T r) {
-    T max = arr[l];
-    for (auto i = l + 1; i < r; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
-        }
-    }
-    return max;
+template <typename T>
+void MergeInsertionSort(vector<T>& a){
+    return MergeSort(a, (T)0, (T)a.size(), cmp<typeof(a[0])>);
 }
+
+/*
+
+    ///////////////////////////////////////
+
+*/
+
+
 
 template<typename T, typename S>
 void countSort(vector<T>& arr, S base, T exp, T l, T r) {
@@ -246,7 +283,7 @@ void RadixSort(vector<T>& a, S base, T l, T r, function<bool(T, T)> &cmp) {
     }
 }
 
-auto solve() {
+int main() {
     //ll n = 2642245;
     auto n = 5;
     random_device rd;
@@ -266,10 +303,10 @@ auto solve() {
     //InsertionSort(a);
     //InsertionSort(a, l, r);
     //InsertionSort(a, l, r, cmp<typeof(a[0])>);
-    QuickSort(a);
+    //QuickSort(a);
     //QuickSort(a, 0, n - 1);
     //QuickSort(a, 0, n - 1, cmp<typeof(a[0])>);
-    //MergeSort(a);
+    MergeSort(a);
     //MergeSort(a, 0, n - 1);
     //MergeSort(a, 0, n - 1, cmp<typeof(a[0])>);
     //MergeInsertionSort(a, m);
@@ -288,17 +325,5 @@ auto solve() {
     for (auto &x : a){
         printf("%d %s", x, " ");
     }
-    return 0;
-}
-
-signed main() {
-    int q = 1;
-    //cin >> q;
-    while (q > 0){
-        --q;
-        solve();
-        cout << endl;
-    }
-    system("PAUSE");
     return 0;
 }
